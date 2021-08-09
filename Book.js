23583d67f5728e9615296b767a856f0e16427cb1
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-08-09 19:50:05 +0300

    Fix JS linter issues

--- Book.js
+++ Book.js
@@ -1,10 +1,9 @@
+const booksArr = [];
 function Book(title, author) {
   this.title = title;
   this.author = author;
 }
-var booksArr = [];
 
-Book.prototype.addBook = function () {
-  console.log(this.title, this.author);
+Book.prototype.addBook = () => {
   booksArr.push({ title: this.title, author: this.author });
 };
--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-// const author = document.getElementById('author-input');
-const form = document.querySelector("#book-form");
-if (localStorage.getItem("booksData") !== null) {
-  var booksData = JSON.parse(localStorage.getItem("booksData"));
-  booksData.map((book) => {
-    const bookContainer = document.createElement("div");
-    const bookTitle = document.createElement("p");
-    const bookAuthor = document.createElement("p");
-    const remove = document.createElement("button");
-    remove.className = "remove";
-    remove.innerText = "Remove";
-    document.querySelector(".list-books").appendChild(bookContainer);
+const form = document.querySelector('#book-form');
+if (localStorage.getItem('booksData') !== null) {
+  const booksData = JSON.parse(localStorage.getItem('booksData'));
+  booksData.forEach((book) => {
+    const bookContainer = document.createElement('div');
+    const bookTitle = document.createElement('p');
+    const bookAuthor = document.createElement('p');
+    const remove = document.createElement('button');
+    remove.className = 'remove';
+    remove.innerText = 'Remove';
+    document.querySelector('.list-books').appendChild(bookContainer);
     bookContainer.appendChild(bookTitle);
     bookContainer.appendChild(bookAuthor);
     bookContainer.appendChild(remove);
     bookTitle.innerText = book.title;
     bookAuthor.innerText = book.author;
 
-    remove.addEventListener("click", (event) => {
+    remove.addEventListener('click', (event) => {
       booksData.splice(booksData.indexOf(book), 1);
-      localStorage.setItem("booksData", JSON.stringify(booksData));
-      var x = event.target.parentNode.remove();
+      localStorage.setItem('booksData', JSON.stringify(booksData));
+      event.target.parentNode.remove();
     });
   });
 }
-document.querySelector(".add").addEventListener("click", (event) => {
+document.querySelector('.add').addEventListener('click', () => {
   const titleInput = form.elements.title.value;
   const authorInput = form.elements.author.value;
 
-  let newbook = new Book(titleInput, authorInput);
+  const newbook = new Book(titleInput, authorInput);
 
-  if (localStorage.getItem("booksData") === null) {
-    localStorage.setItem("booksData", JSON.stringify([]));
+  if (localStorage.getItem('booksData') === null) {
+    localStorage.setItem('booksData', JSON.stringify([]));
   }
-  var oldData = JSON.parse(localStorage.getItem("booksData"));
+  const oldData = JSON.parse(localStorage.getItem('booksData'));
   oldData.push(newbook);
-  localStorage.setItem("booksData", JSON.stringify(oldData));
+  localStorage.setItem('booksData', JSON.stringify(oldData));
 });
