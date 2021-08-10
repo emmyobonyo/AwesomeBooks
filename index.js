9b0f02571d1586b8da87acb176a5883c41db2251
medaminedev66 <md.amine.smahi@gmail.com>
2021-08-10 21:22:29 +0100

    Add renderBooks method to the Books class

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import Book from './module/Book.js';
 const form = document.querySelector('#book-form');
 class Books {
   constructor() {
+    this.booksData = null;
     this.book = null;
     this.id = null;
   }
@@ -20,40 +21,38 @@ class Books {
     booksData.splice(booksData.indexOf(this.id), 1);
     localStorage.setItem('booksData', JSON.stringify(booksData));
   }
+
+  renderBooks() {
+    if (localStorage.getItem('booksData') !== null) {
+      this.booksData = JSON.parse(localStorage.getItem('booksData'));
+      this.booksData.forEach((book, id) => {
+        const bookContainer = document.createElement('div');
+        const bookTitle = document.createElement('p');
+        const bookAuthor = document.createElement('p');
+        const remove = document.createElement('button');
+        remove.className = 'remove';
+        remove.innerText = 'Remove';
+        document.querySelector('.list-books').appendChild(bookContainer);
+        bookContainer.appendChild(bookTitle);
+        bookContainer.appendChild(bookAuthor);
+        bookContainer.appendChild(remove);
+        bookTitle.innerText = book.title;
+        bookAuthor.innerText = book.author;
+        remove.addEventListener('click', (event) => {
+          this.removeBook(id);
+          event.target.parentNode.remove();
+        });
+      });
+    }
+  }
 }
-if (localStorage.getItem('booksData') !== null) {
-  const booksData = JSON.parse(localStorage.getItem('booksData'));
-  booksData.forEach((book, id) => {
-    const bookContainer = document.createElement('div');
-    const bookTitle = document.createElement('p');
-    const bookAuthor = document.createElement('p');
-    const remove = document.createElement('button');
-    remove.className = 'remove';
-    remove.innerText = 'Remove';
-    document.querySelector('.list-books').appendChild(bookContainer);
-    bookContainer.appendChild(bookTitle);
-    bookContainer.appendChild(bookAuthor);
-    bookContainer.appendChild(remove);
-    bookTitle.innerText = book.title;
-    bookAuthor.innerText = book.author;
-    remove.addEventListener('click', (event) => {
-      const removebook = new Books();
-      removebook.removeBook(id);
-      event.target.parentNode.remove();
-    });
-  });
-}
+const booksLibrary = new Books();
+booksLibrary.renderBooks();
 document.querySelector('.add').addEventListener('click', () => {
   const titleInput = form.elements.title.value;
   const authorInput = form.elements.author.value;
   if (localStorage.getItem('booksData') === null) {
     localStorage.setItem('booksData', JSON.stringify([]));
   }
-  const newbook = new Books();
-  newbook.addBook(titleInput, authorInput);
-});
-Array.from(document.querySelectorAll('.remove')).forEach((remove) => {
-  remove.addEventListener('click', (event) => {
-    event.target.parentNode.remove();
-  });
+  booksLibrary.addBook(titleInput, authorInput);
 });
